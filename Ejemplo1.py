ab24759a0765e149e4550146bf1707b1fc39c639
w580ton <w580ton@gmail.com>
2017-02-22 20:30:05 -0600

    Ejemplo1
    Primera practica GitHub Python2.7

---
+++ Ejemplo1.py
@@ -0,0 +1,8 @@
+#Creando un ejemplo en github, comenzando con un sencillo paso
+#Codigo python(2.7) basico
+print "Hola mundo, este es mi primer ejemplo GitHub"
+print "Sumaremos 2 numeros dados por el usuario"
+a=int(input("Dame primero num:")
+b=int(input("Dame segundo num:")
+res=a+b
+print "Tu resultado es:",res
